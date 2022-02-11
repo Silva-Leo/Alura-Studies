e1ec10f2248392a75f061daa3332961c60fc1b89
Leo Silva <devleonardosilva@gmail.com>
2022-02-11 20:59:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Botao from "./components/Botao/index"
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Botao/>      
+    </div>
+  )
+}
+
+export default App
